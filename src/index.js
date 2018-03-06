18659dfce11ff812dafa8ca139e860b23ebc417a
zhaoxizheng <zhaoxi@umich.edu>
2018-03-06 13:32:37 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
+    // For browser based projects, there are <BrowserRouter> and <HashRouter> components. 
+    // The <BrowserRouter> should be used when you have a server that will handle dynamic requests (knows how to respond to any possible URI), 
+    // while the <HashRouter> should be used for static websites (can only respond to requests for files that it knows about).
+    <BrowserRouter>  
         <App />
     </BrowserRouter>,
     document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker(); // For offline perfermance.
 
